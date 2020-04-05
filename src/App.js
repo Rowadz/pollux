65cbc498e44b26f3ae5f79156006fe4e7a2a78d9
Rowad <rowadwo@gmail.com>
2020-04-05 17:55:20 +0300

    shameless plug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
+import { GithubOutlined, LinkedinOutlined } from '@ant-design/icons';
+
 import { Button, Layout, Menu, Breadcrumb } from 'antd';
 const { Header, Content, Footer } = Layout;
 
@@ -33,7 +35,24 @@ function App() {
           </div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>
-          Ant Design ©2018 Created by Ant UED
+          Made by <b>Rowad</b>
+          <a
+            href="https://github.com/MohammedAl-Rowad"
+            target="_blank"
+            without
+            rel="noopener noreferrer"
+          >
+            <GithubOutlined style={{ fontSize: '20px', color: '#08c' }} />
+          </a>
+          <b>/</b>
+          <a
+            href="https://www.linkedin.com/in/mohammed-al-rowad/"
+            target="_blank"
+            without
+            rel="noopener noreferrer"
+          >
+            <LinkedinOutlined style={{ fontSize: '20px', color: '#08c' }} />
+          </a>
         </Footer>
       </Layout>
     </main>
