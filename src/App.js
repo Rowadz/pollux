c5d217b7c79b1aa578284ace2e9f4f0820d3140d
Rowad <rowadwo@gmail.com>
2020-04-05 17:39:01 +0300

    simple structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React, { useState } from 'react';
 import './App.css';
+import { Button, Layout, Menu, Breadcrumb } from 'antd';
+const { Header, Content, Footer } = Layout;
 
 function App() {
   const [json, setJson] = useState('init 2');
   return (
     <main>
-      <h1>{json}</h1>
+      <Layout className="layout" theme="dark">
+        <Header>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['1']}
+            style={{ textAlign: 'center' }}
+          >
+            <Menu.Item key="1">Pollux</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>
+              <b>Editor</b>
+            </Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-content">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae
+            omnis dolore mollitia aliquid saepe rem aperiam non culpa suscipit
+            nemo et animi, nulla nobis reiciendis? Consectetur tempora
+            aspernatur dolores adipisci!
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
     </main>
   );
 }
