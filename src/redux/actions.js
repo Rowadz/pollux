f885e18421ddca58431bc6802b95b4a0f525af27
MohammedAl-rowad <rowadwo@gmail.com>
2020-06-20 17:29:17 +0300

    adding types to actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -7,32 +7,68 @@ import {
   DELETE_PROP,
 } from './actionTypes'
 
+/**
+ *
+ * @param {Object} model - the model object
+ * @param {string} model.name - the model name
+ * @param {string} model.id - the model uuid
+ * @param {Date} model.createdAt - the mode timestamp
+ */
 export const addModel = (model) => ({
   type: ADD_MODEL,
   payload: model,
 })
 
+/**
+ *
+ * @param {string} uuid - the prop uuid
+ *
+ */
 export const deleteModel = (uuid) => ({
   type: DELETE_MODEL,
   payload: uuid,
 })
 
-export const addPropName = (uuidAndPropName) => ({
+/**
+ *
+ * @param {Object} props - the props object
+ * @param {string} props.uuid - the props uuid
+ * @param {string} props.propName - the props name
+ */
+export const addPropName = (props) => ({
   type: ADD_RPOP_NAME,
-  payload: uuidAndPropName,
+  payload: props,
 })
 
+/**
+ *
+ * @param {string} uuid - the prop uuid
+ *
+ */
 export const removeAllProps = (uuid) => ({
   type: REMOVE_ALL_RPOPS,
   payload: uuid,
 })
 
-export const editProp = (uuidAndNewName) => ({
+/**
+ * @param {Object} props - the prop object
+ * @param {string} props.propId - the prop uuid
+ * @param {string} props.id - the model uuid
+ * @param {string} props.newName - the new prop name
+ * @param {(input?: any) => any} props.func - the faker function
+ */
+export const editProp = (props) => ({
   type: EDIT_PROP,
-  payload: uuidAndNewName,
+  payload: props,
 })
 
-export const delProp = (uuidToModelAndProp) => ({
+/**
+ *
+ * @param {Object} props - the prop object
+ * @param {string} props.propId - the prop uuid
+ * @param {string} props.modelId - the model uuid
+ */
+export const delProp = (props) => ({
   type: DELETE_PROP,
-  payload: uuidToModelAndProp,
+  payload: props,
 })
