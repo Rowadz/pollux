822590f8c74aa6fe638b6ed5612e321d86a014bf
MohammedAl-rowad <rowadwo@gmail.com>
2020-06-21 21:20:18 +0300

    maybe a fix for #6

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import 'rsuite/dist/styles/rsuite-dark.css'
 import './App.css'
 import { Header, Content, Footer, Generate } from './components/'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
