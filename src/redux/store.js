0b54f5b4261471783492663aa52d154e5142f4e1
MohammedAl-rowad <rowadwo@gmail.com>
2020-06-23 01:14:43 +0300

    for #9

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore } from 'redux'
 import rootReducer from './reducers'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-export default createStore(rootReducer)
+export default createStore(rootReducer, composeWithDevTools())
