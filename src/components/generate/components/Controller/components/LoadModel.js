f0815dd43cfcedaa4eae3972ff83f426a2274ba9
MohammedAl-rowad <rowadwo@gmail.com>
2020-08-01 14:38:27 +0300

    listing all models as json #1

--- src/components/generate/components/Controller/components/LoadModel.js
+++ src/components/generate/components/Controller/components/LoadModel.js
@@ -1,18 +1,28 @@
 import React, { useState } from 'react'
-import { IconButton, Icon, Modal, Button, Checkbox } from 'rsuite'
-import { connect } from 'react-redux'
-import emptySave from './emptySave.svg'
+import { IconButton, Icon, Modal, Button, Divider } from 'rsuite'
 import { Alert } from 'rsuite'
 const { Header, Body, Footer, Title } = Modal
 
 export default function LoadModel() {
-  const [state, setState] = useState({ showTheModalOfModels: false })
+  const [state, setState] = useState({
+    showTheModalOfModels: false,
+    models: [],
+  })
   const close = () => setState({ ...state, showTheModalOfModels: false })
+  const open = () =>
+    setState({
+      ...state,
+      showTheModalOfModels: true,
+      models: JSON.parse(localStorage.getItem('models')) || [],
+    })
+  const load = (model) => {
+    console.log(model)
+  }
   return (
     <section style={{ display: 'inline', marginLeft: 10 }}>
       <IconButton
         color="yellow"
-        onClick={() => setState({ ...state, showTheModalOfModels: true })}
+        onClick={open}
         circle
         icon={<Icon icon="tasks" />}
       />
@@ -24,7 +34,24 @@ export default function LoadModel() {
         <Header>
           <Title>Load your saved models!</Title>
         </Header>
-        <Body></Body>
+        <Body>
+          {state.models.map((m, i) => (
+            <div key={i}>
+              <h3>
+                <strong>{m.name}</strong>
+              </h3>
+              <Button
+                style={{ float: 'right' }}
+                appearance="ghost"
+                onClick={() => load(m)}
+              >
+                Click to Load {m.name}
+              </Button>
+              <pre>{JSON.stringify(m, null, 2)}</pre>
+              <Divider />
+            </div>
+          ))}
+        </Body>
         <Footer>
           <Button onClick={close} appearance="subtle">
             Close
