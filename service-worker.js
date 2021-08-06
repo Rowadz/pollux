e2014934f9f847f209d8335e5c0c5864bd5bc759
MohammedAl-rowad <rowadwo@gmail.com>
2021-08-07 00:23:11 +0300

    Add the enable disable auth toggle into the tour

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pollux/precache-manifest.6d56891627a39248e49d3a96a4689a2d.js"
+  "/pollux/precache-manifest.6e941db71960d5149739da4dfd023fa3.js"
 );
 
 self.addEventListener('message', (event) => {
