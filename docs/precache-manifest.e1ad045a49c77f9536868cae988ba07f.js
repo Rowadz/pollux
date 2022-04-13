b992ffe5a37fbd834ef341e423c63e93f2d6f8d0
MohammedAl-rowad <rowadwo@gmail.com>
2022-04-13 03:43:19 +0200

    Deploy after cleanup

---
+++ docs/precache-manifest.e1ad045a49c77f9536868cae988ba07f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18dc115c26e52ca897bf0d58953fe4ad",
+    "url": "/pollux/index.html"
+  },
+  {
+    "revision": "09d4f85209d3ec57cb58",
+    "url": "/pollux/static/css/2.214fe3a2.chunk.css"
+  },
+  {
+    "revision": "238d0a49d6081fde85a6",
+    "url": "/pollux/static/css/main.45384ce1.chunk.css"
+  },
+  {
+    "revision": "09d4f85209d3ec57cb58",
+    "url": "/pollux/static/js/2.6af23d09.chunk.js"
+  },
+  {
+    "revision": "e04c726df601fc820617ae0c3455793e",
+    "url": "/pollux/static/js/2.6af23d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "238d0a49d6081fde85a6",
+    "url": "/pollux/static/js/main.74582689.chunk.js"
+  },
+  {
+    "revision": "75eef6d78ae3f668e0c3",
+    "url": "/pollux/static/js/runtime-main.5df1e606.js"
+  },
+  {
+    "revision": "d0bcb7b7f28cf9727e3c7c08409821f1",
+    "url": "/pollux/static/media/box.d0bcb7b7.svg"
+  },
+  {
+    "revision": "4333af263562fc23b3811db25d6e0246",
+    "url": "/pollux/static/media/cat.4333af26.svg"
+  },
+  {
+    "revision": "7448158e39aa3c9f2a6681d116ace6db",
+    "url": "/pollux/static/media/empty.7448158e.svg"
+  },
+  {
+    "revision": "100321226a02ab5a028bc4cfbe73d506",
+    "url": "/pollux/static/media/emptySave.10032122.svg"
+  },
+  {
+    "revision": "ac13338d2e8245ab4959fb344b73ba26",
+    "url": "/pollux/static/media/features.ac13338d.svg"
+  },
+  {
+    "revision": "a2b0ac7738df7705aa2712b7f07b5f5e",
+    "url": "/pollux/static/media/loadModels.a2b0ac77.svg"
+  },
+  {
+    "revision": "ea2fca44d51d2298323965f6dd1395cb",
+    "url": "/pollux/static/media/prototype.ea2fca44.svg"
+  },
+  {
+    "revision": "f4e51a2920e26618a8cd4916fdd2431f",
+    "url": "/pollux/static/media/prototype2.f4e51a29.svg"
+  }
+]);
