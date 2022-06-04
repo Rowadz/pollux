1cb7f72031fcf1eac7a919f3c3332894768bb3a7
MohammedAl-rowad <rowadwo@gmail.com>
2022-06-04 03:59:13 +0200

    Convert more files to ts

--- src/zipFileContent/package.json.ts
+++ src/zipFileContent/package.json.ts
@@ -1,13 +1,15 @@
-export default (auth=false) => ({
+export default (auth = false) => ({
   description:
     'The file is generated using https://mohammedal-rowad.github.io/pollux/',
   scripts: {
-    dev: `${auth ? 'json-server-auth' : 'json-server'} --watch db.json -r routes.json`,
+    dev: `${
+      auth ? 'json-server-auth' : 'json-server'
+    } --watch db.json -r routes.json`,
   },
   author: 'Rowadz',
   license: 'MIT',
   dependencies: {
     'json-server': '^0.16.2',
-    'json-server-auth': '^2.0.2'
+    'json-server-auth': '^2.0.2',
   },
 })
--- src/zipFileContent/readme.md.ts
+++ src/zipFileContent/readme.md.ts
@@ -1,4 +1,4 @@
-export default name => `# Generated json-server API for ${name} model
+export default (name: string) => `# Generated json-server API for ${name} model
 This json-server api was generated via [pollux](https://mohammedal-rowad.github.io/pollux/)
 and it runs using [json-server](https://github.com/typicode/json-server), please read json-server docs for more info.
 
